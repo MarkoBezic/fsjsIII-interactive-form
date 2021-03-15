03d54f2387a8c021f90639ec2681c9ac50a1fbcf
markobezic <marko.bezic@gmail.com>
2021-03-15 13:41:52 -0400

    added functionality to display total cost of activities when they are checked or unchecked

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ const selectJobRoleEl = document.querySelector("#title");
 const selectColorEl = document.querySelector("#color");
 const selectDesignEl = document.querySelector("#design");
 const colorOptions = document.querySelectorAll("[data-theme]");
+const registerForActivities = document.querySelector("#activities");
+let totalActivitiesCost = 0;
 
 //Set name field to focus state on page load
 inputNameEl.setAttribute("focus", true);
@@ -22,26 +24,39 @@ selectJobRoleEl.addEventListener("change", () => {
 });
 
 //Disable select color field on page load
-selectColorEl.setAttribute("disabled", true);
+selectColorEl.disabled = true;
 
 //Listen for design selection and update available colors accordingly
 selectDesignEl.addEventListener("change", () => {
-  selectColorEl.removeAttribute("disabled");
-  if (selectDesignEl.value === "js puns") {
+  selectColorEl.disabled = false;
+  if (selectDesignEl[0].selected) {
     for (let i = 0; i < colorOptions.length; i++) {
       const dataTheme = colorOptions[i].getAttribute("data-theme");
-      colorOptions[i].removeAttribute("hidden");
+      colorOptions[i].hidden = false;
       if (dataTheme !== "js puns") {
-        colorOptions[i].setAttribute("hidden", true);
+        colorOptions[i].hidden = true;
       }
     }
-  } else if (selectDesignEl.value === "heart js") {
+  } else if (selectDesignEl[1].selected) {
     for (let i = 0; i < colorOptions.length; i++) {
       const dataTheme = colorOptions[i].getAttribute("data-theme");
-      colorOptions[i].removeAttribute("hidden");
+      colorOptions[i].hidden = false;
       if (dataTheme !== "heart js") {
-        colorOptions[i].setAttribute("hidden", true);
+        colorOptions[i].hidden = true;
       }
     }
   }
+});
+
+//When activity checked/unchecked update total accordingly
+registerForActivities.addEventListener("change", (e) => {
+  const individualActivityCost = e.target.getAttribute("data-cost");
+  const activitiesCostEl = document.querySelector(".activities-cost");
+
+  if (e.target.checked) {
+    totalActivitiesCost += Number(individualActivityCost);
+  } else {
+    totalActivitiesCost -= Number(individualActivityCost);
+  }
+  activitiesCostEl.innerHTML = `Total: $${totalActivitiesCost}.00`;
 });
